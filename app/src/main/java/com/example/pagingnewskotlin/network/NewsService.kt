2581325ecc58f3e72e08306334ce0580b5d338f4
Ananda Muhamad Lukman <v-ananda.lukman@anteraja.id>
2021-12-07 16:26:33 +0700

    Setup Up Library, and Adding Config Network

---
+++ app/src/main/java/com/example/pagingnewskotlin/network/NewsService.kt
@@ -0,0 +1,4 @@
+package com.example.pagingnewskotlin.network
+
+interface NewsService {
+}
---
+++ app/src/main/java/com/example/pagingnewskotlin/network/ResApi.kt
@@ -0,0 +1,30 @@
+package com.example.pagingnewskotlin.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ResApi {
+
+    val baseUrl = ""
+
+    fun restApi() : NewsService {
+        // Step 1 configurasi interceptor
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val okHttp = OkHttpClient().newBuilder().addInterceptor(interceptor).build()
+
+        // Step 2 build retrofit config
+        val retrofit = Retrofit.Builder().baseUrl(baseUrl)
+            .client(okHttp)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build()
+
+        // Step 3 include configurasi retrofit ke interface
+        return retrofit.create(NewsService::class.java)
+    }
+}
